8aa411fc149c71bc796c0b48fc7a0371e5c4417e
Md Emon <mdshajdulhaqueemon@gmail.com>
2023-12-22 17:46:34 +0600

    update the data for data base

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express=require('express')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app=express()
 const cors=require('cors')
 
@@ -32,26 +32,87 @@ async function run() {
     const teamCollection=client.db('To-do-application').collection('team-member')
     const testimonialCollection=client.db('To-do-application').collection('testimonials')
     const contactCollection=client.db('To-do-application').collection('contacts')
+    const taskCollection=client.db('To-do-application').collection('task-gallary')
+    const addTaskCollection=client.db('To-do-application').collection('addTask')
     
+    //Team section
     app.get('/teams',async(req,res)=>{
   
         const result=await teamCollection.find().toArray()
         res.send(result)
       })
 
+      //Testimonials section
     app.get('/users',async(req,res)=>{
   
         const result=await testimonialCollection.find().toArray()
         res.send(result)
       })
 
+      //Contact section
     app.get('/contact',async(req,res)=>{
   
         const result=await contactCollection.find().toArray()
         res.send(result)
       })
 
+      //Task section
+    app.get('/taskGallary',async(req,res)=>{
+  
+        const result=await taskCollection.find().toArray()
+        res.send(result)
+      })
 
+      //Added a task by mongodb 
+
+      app.post('/addTask',async(req,res)=>{
+        const items=req.body;
+        const result=await addTaskCollection.insertOne(items)
+        res.send(result)
+      })
+
+      //Get Task
+      app.get('/getTask',async(req,res)=>{
+  
+        const result=await addTaskCollection.find().toArray()
+        res.send(result)
+      })
+
+      app.delete('/deleteTask/:id',async(req,res)=>{
+        const id=req.params.id;
+        const query={_id: new ObjectId(id)}
+      
+        const result=await addTaskCollection.deleteOne(query)
+        res.send(result)
+      })
+
+      app.get('/tasks/:id',async(req,res)=>{
+        const id=req.params.id;
+        const query={_id:new ObjectId(id)};
+        const result=await addTaskCollection.findOne(query)
+        res.send(result)
+  
+      })
+      app.put('/tasks/:id',async(req,res)=>{
+        const id=req.params.id;
+        const filter={_id:new ObjectId(id)}
+        const options = { upsert: true };
+        const upDateTask=req.body;
+        const task = {
+          
+          $set: {
+            name:upDateTask.name,
+            task_name:upDateTask.task_name,
+            date:upDateTask.date,
+            
+             priority:upDateTask.priority,
+             
+          },
+        
+        };
+        const result = await addTaskCollection.updateOne(filter, task, options);
+        res.send(result)
+      })
 
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
