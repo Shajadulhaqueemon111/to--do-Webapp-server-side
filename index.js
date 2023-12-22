6bb7daa5d027be3274fe84b69415b27e7d4c90a4
Md Emon <mdshajdulhaqueemon@gmail.com>
2023-12-22 18:32:46 +0600

    useses cors origine

--- index.js
+++ index.js
@@ -8,6 +8,17 @@ require('dotenv').config()
 const port=process.env.PORT || 5000;
 
 //middle wire
+app.use(cors({
+  origin:[
+   'http://localhost:5173',
+   'https://toothsome-cellar.surge.sh',
+    'https://task-management-system-8c466.firebaseapp.com',
+    'https://task-management-system-8c466.web.app'
+  ],
+  credentials:true
+}));
+app.use(express.json())
+app.use(cors())
 
 app.use(express.json())
 app.use(cors())
